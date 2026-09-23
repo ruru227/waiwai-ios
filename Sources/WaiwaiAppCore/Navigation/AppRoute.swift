6dff6ae27da31af88c0aad25e59e551e25858908
ruru227 <apurikaifayong777@gmail.com>
2026-09-24 08:51:02 +0900

    Add SwiftUI app foundation

---
+++ Sources/WaiwaiAppCore/Navigation/AppRoute.swift
@@ -0,0 +1,3 @@
+public enum AppRoute:Hashable,Sendable {
+ case comedianProfile(Comedian),callType(Comedian),instantPayment(Comedian),ringing(Comedian),videoCall(Comedian),callComplete(Comedian),reservationCalendar(Comedian),payment(Comedian),reservationComplete(Comedian),letterGift(Comedian)
+}
