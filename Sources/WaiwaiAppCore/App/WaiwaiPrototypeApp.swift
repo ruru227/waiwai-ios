fd9b25dfa685e30f595f7c1d7757e43b5cb4deb6
ruru227 <apurikaifayong777@gmail.com>
2026-09-24 08:58:25 +0900

    Use interactive prototype after mock login

--- Sources/WaiwaiAppCore/App/WaiwaiPrototypeApp.swift
+++ Sources/WaiwaiAppCore/App/WaiwaiPrototypeApp.swift
@@ -1,2 +1,12 @@
 import SwiftUI
-public struct WaiwaiPrototypeAppView:View{@State private var authenticated=false;public init(){};public var body:some View{if authenticated{WaiwaiMainTabView()}else{LoginView{authenticated=true}}}}
+public struct WaiwaiPrototypeAppView:View{
+ @State private var authenticated=false
+ public init(){}
+ public var body:some View{
+  if authenticated {
+   InteractivePrototypeView()
+  } else {
+   LoginView { authenticated=true }
+  }
+ }
+}
